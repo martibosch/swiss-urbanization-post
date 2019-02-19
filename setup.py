77ee345896bc339a954000bc7c78b83dc54f1a6a
Martí Bosch <marti.bosch@epfl.ch>
2019-02-19 11:11:24 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+_description = 'Materials to reproduce the blog post of http://martibosch' \
+               '.github.io/managing-analysis-workflows-geodata/'
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=_description,
+    author='Martí Bosch',
+    license='',
+)
